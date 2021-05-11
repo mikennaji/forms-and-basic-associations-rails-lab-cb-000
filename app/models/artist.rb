eb875f03822fd175a6a66cc84d5808fb623c53a7
Michael Nnaji <mnnaji3@uwo.ca>
2021-05-11 16:48:56 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,14 @@
 class Artist < ActiveRecord::Base
   # add associations here
+  has_many :songs
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+end
+
+
+def artist_name
+  self.artist ? self.artist.name : nil
+end
+
 end
